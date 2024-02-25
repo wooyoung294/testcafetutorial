d595b101bc157811463dc9f3d3cbc2a74d10b97d
wooyoung <andy241822@gmail.com>
2024-02-25 09:59:22 +0900

    report.json 생성 테스트

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'reports')));
 app.use(express.static(path.join(__dirname, 'cucumber-json-reports')));
 app.use('/', indexRouter);
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://34.47.73.82, http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
